7ccb1ce99a46a1359da59c0cbad3e308ed3beae2
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2021-08-17 19:48:36 +0900

    small refactor

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -2,9 +2,7 @@ package com.example.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories
 @SpringBootApplication
 public class TodoApplication {
 
